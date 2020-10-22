8adea6aa2734406da6d99b5e9e9590e7580755a8
xb0or <57210034+xb0or@users.noreply.github.com>
2020-10-22 06:52:34 +0000

    Update kd-ddns.sh

--- kd-ddns.sh
+++ kd-ddns.sh
@@ -7,13 +7,14 @@ kd-ddns(){
     read -p "请输入主域名(eg:example.com) :" zhuyuming
     read -p "请输入子域名(eg:123.example.com 只需填入123) :" ziyuming
     read -p "请输入 cloudflare API :" youkey
-	read -p "请输入 cloudflare Username:" youuser
-
+    read -p "请输入 cloudflare Username:" youuser
+    
+    wget https://raw.githubusercontent.com/xb0or/KD-DDNS/main/cf-ddns.sh && chmod +x cf-ddns.sh
 	
     sed -i "s/zhuyuming/${zhuyuming}/g" /usr/local/bin/cf-ddns.sh
     sed -i "s/ziyuming/${ziyuming}.${zhuyuming}/g" /usr/local/bin/cf-ddns.sh
     sed -i "s/youkey/${youkey}/g" /usr/local/bin/cf-ddns.sh
-	sed -i "s/youuser/${youuser}/g" /usr/local/bin/cf-ddns.sh
+    sed -i "s/youuser/${youuser}/g" /usr/local/bin/cf-ddns.sh
 
     cf-ddns.sh
     
